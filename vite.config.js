a6372196d5ca9922037937a044ed2ade832b946c
PrakashMakwana007 <pasatvara7777@gmail.com>
2025-07-10 09:39:55 +0530

    $404

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import lineClamp from '@tailwindcss/line-clamp'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    
     tailwindcss(),
-    // lineClamp(),
-    react()],
+    react(),
+  ],
+  server: {
+    historyApiFallback: true, 
+  }
 })
